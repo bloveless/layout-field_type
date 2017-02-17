60682f7d21a35008f3b773b9b8c0c91dde1a12dd
Brennon Loveless <brennon.loveless@gmail.com>
2017-02-16 19:22:32 -0700

    Add skip to add widget button at the top of the layout field type since it can get rather large.

--- resources/lang/en/message.php
+++ resources/lang/en/message.php
@@ -1,5 +1,7 @@
 <?php
 
 return [
-    'choose_widget' => 'Which widget would you like to add?'
+    'choose_widget'      => 'Which widget would you like to add?',
+    'add_widget'         => 'Add Widget',
+    'skip_to_add_widget' => 'Skip To Add Widget',
 ];
